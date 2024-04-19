30c43f915d237eab3353709a7941767c093b344a
aseatari <aseatari@gmail.com>
2024-04-19 17:14:42 +0300

    Swich library type to dylib

---
+++ neurosdk2.xcframework/ios-arm64/Headers/neurosdk.h
@@ -0,0 +1,20 @@
+#import "NTScanner.h"
+#import "NTSensor.h"
+
+#import "NTBrainBit.h"
+
+
+#import "NTBrainBitBlack.h"
+
+
+#import "NTCallibri.h"
+
+
+
+
+
+
+
+#import "NTBrainBit2.h"
+
+#import "NTTypes.h"
