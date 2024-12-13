a5819ac4c69889258df08a2f9dec96fdc262e03f
aseatari <aseatari@gmail.com>
2024-12-13 17:17:54 +0300

    Added support of DragonEEG

---
+++ ios/neurosdk2.xcframework/ios-arm64-simulator/neurosdk2.framework/Headers/neurosdk.h
@@ -0,0 +1,23 @@
+#import "NTScanner.h"
+#import "NTSensor.h"
+
+#import "NTBrainBit.h"
+
+
+#import "NTBrainBitBlack.h"
+
+
+#import "NTCallibri.h"
+
+
+
+
+
+
+#import "NTNeuroEEG.h"
+#import "NTNeuroEEGSignalProcessing.h"
+
+
+#import "NTBrainBit2.h"
+
+#import "NTTypes.h"
